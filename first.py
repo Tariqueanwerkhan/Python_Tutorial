fb90e59a347ddb7918a8511091d9a0977e469f7f
Tarique Anwer Khan <110827286+Tariqueanwerkhan@users.noreply.github.com>
2025-10-15 08:47:30 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,17 @@
+print("Hello world")
+
+
+# this is a variable
+Instructor = 'Tarique Anwer khan'
+print("Python by", Instructor, sep=':- ')
+
+print(5+3)
+
+b = 5
+c = 7
+print(b+c)
+print(b-c)
+print(b*c)
+print(b/c)
+print(b**c)
+print(b%c)
