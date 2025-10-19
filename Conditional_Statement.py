bd69e9fdd79740c0eecf55df9480a7142a2b8906
Tarique Anwer Khan <110827286+Tariqueanwerkhan@users.noreply.github.com>
2025-10-19 14:16:34 +0530

    Add files via upload

---
+++ Conditional_Statement.py
@@ -0,0 +1,58 @@
+# conditional statement
+# if statement
+# a = 24
+# b = 90
+# if b > a:
+#     print("b is greater than a")
+
+# age = int (input("Enter your age: "))
+# if age > 18:
+#     print("You are an adult.")
+
+
+# if-else condition
+# age = int (input("Enter your age: "))
+# if age > 18:
+#     print("You are an adult.")
+# else:
+#     print("You're a teenager.")
+
+
+# temperature = 30
+# if temperature > 25:
+#     print("It's a hot day.")
+# else:
+#     print("It's not a hot day.")
+
+
+# else-if condition
+# score = int(input("Marks of students out of 100: "))
+# if score >= 90:
+#     print("Grade - A")
+# elif score >= 80:
+#     print("Grade - B")
+# elif score >= 70:
+#     print("Grade - C")
+# else:
+#     print("Grade - D")
+
+
+
+# Nested if-else statement
+# number = int(input("Enter a number: "))
+# if number > 0:
+#     if number % 2 == 0:
+#         print("The number is positive and even.")
+#     else:
+#         print("The number is positive and odd.")
+# else:
+#     if number == 0:
+#         print("The number is zero.")
+#     else:
+#         print("The number is negative.")
+
+
+# conditional expressions
+age = 16
+status = "Adult" if age >= 18 else "Minor"
+print(status)
