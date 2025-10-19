291daa32c3095e4815ea6178c64a55387dbcd5c3
Tarique Anwer Khan <110827286+Tariqueanwerkhan@users.noreply.github.com>
2025-10-19 14:17:18 +0530

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,48 @@
+# variables in python
+a = 1
+print(a)
+
+b = 1.1357101
+print(b)
+
+c = "mahesh"
+print(c)
+
+rajesh = 30
+d = rajesh
+print(d)
+
+x = 55            # adding a number to a variable
+print(x+10)
+
+p,q,r = 11,12,13  # assign multiple values
+print(p,q,r)
+
+# wways to declare a varibale name
+MyName = "Tarique"  #pascal case
+myName = "Tarique"  # camel case
+myname = "Tarique"  # flat case
+my_name = "Tarique" # snake case
+
+
+# rules to declare a variable name
+# 1. variable name must start with: letter or _
+_my_name = "Tarique Anwer Khan"
+print(_my_name)
+
+#2. variable name can contain: letter, numbers, underscore(_)
+my_name = "xyz"
+r1 = 20
+_1 = 2
+
+#3. variables are casesensitive
+myname = "xyz"
+print(myname)
+
+#4. variables name can't be a reserved word
+# for = 20     # for is a reserved word in python
+
+
+
+
+
