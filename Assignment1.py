788488504e6801ef05210e41c0bebb3302efec52
Tarique Anwer Khan <110827286+Tariqueanwerkhan@users.noreply.github.com>
2025-10-20 08:47:58 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,16 @@
+# print("Hello world")
+# print('Hello world')
+# print("you're a good man")
+# print('''you're a "good" man''')
+
+print("Python is fun.")
+print('''"Quotes" and 'single quotes' can be tricky.''')
+print("Python is fun.\n\"Quotes\" and 'single quotes' can be tricky.")
+
+
+name = "Tarique Anwer Khan"
+age = 24
+city = "Noida"
+print("My name is", name, "I am", age, "years old", "and i live in", city)
+print(f"My name is {name} I am {age} years old and i live in {city}")
+
