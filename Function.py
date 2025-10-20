5c350fb1909429ed78d54716554e252eb76bd709
Tarique Anwer Khan <110827286+Tariqueanwerkhan@users.noreply.github.com>
2025-10-20 08:49:13 +0530

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,63 @@
+# functions in python
+
+# create function without using parameter
+# def greetings():
+#     print("Welcome to the Tarique Anwer Khan World!")
+#
+# # call function
+# greetings()
+
+
+# create a function to add 2 numbers using parameter
+# def add2numbers(a,b):             # parameter (a,b)
+#     result = a + b
+#     print("The sum is:",result)
+# #call above sum function
+# add2numbers(5,3)         # arguments
+# add2numbers(10,100)
+
+
+# create a function to add 3 numbers
+# def add3numbers(a,b,c):
+#     result = a + b + c
+#     print("The sum is:",result)
+# #call above sum function
+# add3numbers(5,3, 100)
+
+# function with return statement
+# def add2num(a,b):
+#     return a+b
+# # after return statement function end
+# sum2num = add2num(10,1)
+# print(sum2num)
+
+
+# function to convert celsius to fahrenheit- using return statement
+# def celsius_to_fahrenheit(celsius):
+#     farenheit = (9 / 5) * celsius + 32
+#     return farenheit
+# # calling this function to return a value
+# temp_f = celsius_to_fahrenheit(25)
+# print(temp_f)
+# print("with return: ", type(temp_f))
+
+
+
+# function to convert celsius to fahrenheit- without return
+# def celsius_to_fahrenheit(celsius):
+#     farenheit = (9 / 5) * celsius + 32
+#     print(farenheit)
+# # calling this function to return a value
+# temp_f2 = celsius_to_fahrenheit(50)
+# print("without return: ", type(temp_f2))
+
+
+# pass statement
+def anything():
+    pass
+
+def top_right_CTA_button_type():
+    pass
+
+print("Hello World!")
+
