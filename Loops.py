5fc3ddf4ce222ceb0a7cb2e7f96aba3e3f5168e9
Tarique Anwer Khan <110827286+Tariqueanwerkhan@users.noreply.github.com>
2025-10-22 17:52:55 +0530

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,92 @@
+# Loops in Python - while & for loop
+# while loop
+
+# count = 0
+# while count < 5:
+#     print(count)
+#     count += 1
+
+
+# print numbers from 1 to 5 using while loop
+# count = 1
+# while count < 6:
+#     print(count)
+#     count += 1
+
+# count = 5
+# while count > 0:
+#     print(count)
+#     count -= 1
+# else:
+#     print("While Loop Ended")
+# # check conditions to avoid infinite loop
+
+
+# for loop
+# language = "Python"
+# for x in language:
+#     print(x)
+
+# range function
+# range(stop)
+# range(start, stop)
+# range(start, stop, step)
+
+# for i in range(10):
+#     print(i)
+
+# for i in range(5,10):
+#     print(i)
+
+# for i in range(1,10,3):
+#     print(i)
+
+# for i in range(5):
+#     print(i)
+# else:
+#     print("for loop ended")
+
+
+# loop control statements
+# 1. pass statement
+
+# count = 5
+# while count > 0:
+#     if count == 3:
+#         pass
+#     else:
+#         print(count)
+#     count = count - 1
+
+
+# 2. break statement
+# for i in range(10):
+#     if i == 6:
+#         break
+#     print(i)
+
+
+# 3. continue statement
+# for i in range(10):
+#     if i == 5:
+#         continue
+#     print(i)
+
+
+# pass statement vs continue statement
+count = 5
+while count > 0:
+    if count == 3:
+        pass
+    else:
+        print(count)
+    count -= 1
+
+
+
+
+
+
+
+
+
